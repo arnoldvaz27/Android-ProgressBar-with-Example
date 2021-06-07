f28018eb1787e34ee65176f806499a050b53216d
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-07 18:33:56 +0530

    ProgressBar Project added to git. This project shows how to use ProgressBar in Android using Java.

---
+++ app/src/main/java/com/example/differentandroidcodes/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.differentandroidcodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
